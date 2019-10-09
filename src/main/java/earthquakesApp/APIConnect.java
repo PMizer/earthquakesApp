a5884fddd207f52b341a0facd0474b3c0b0ed768
pmzier <mizer.piotr@gmail.com>
2019-10-10 01:51:14 +0200

    added connection to api

---
+++ src/main/java/earthquakesApp/APIConnect.java
@@ -0,0 +1,31 @@
+package earthquakesApp;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class APIConnect {
+
+    public static JsonObject apiConnection() throws IOException {
+
+        String sURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"; //just a string
+
+        // Connect to the URL using java's native library
+        URL url = new URL(sURL);
+        URLConnection request = url.openConnection();
+        request.connect();
+
+        // Convert to a JSON object to print data
+        JsonParser jp = new JsonParser(); //from gson
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+        JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
+
+        return  rootobj;
+    }
+}
--- src/main/java/earthquakesApp/Main.java
+++ src/main/java/earthquakesApp/Main.java
@@ -2,15 +2,14 @@ package earthquakesApp;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+
 import earthquakesApp.geoJson.EarthshakedJson;
 import earthquakesApp.geoJson.Feature;
-import earthquakesApp.geoJson.Geometry;
-import org.json.JSONException;
 
-import java.util.ArrayList;
+
+
 import java.util.List;
-import java.util.Scanner;
+
 
 
 public class Main {
@@ -20,8 +19,15 @@ public class Main {
 
     private static void deserializeGeoJson(){
 
-        String response = "{\"type\":\"FeatureCollection\",\"metadata\":{\"generated\":1570553726000,\"url\":\"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson\",\"title\":\"USGS Significant Earthquakes, Past Week\",\"status\":200,\"api\":\"1.8.1\",\"count\":2},\"features\":[{\"type\":\"Feature\",\"properties\":{\"mag\":3.54,\"place\":\"5km WSW of Colma, CA\",\"time\":1570290066700,\"updated\":1570553279832,\"tz\":-480,\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/nc73286930\",\"detail\":\"https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/nc73286930.geojson\",\"felt\":12427,\"cdi\":4.2999999999999998,\"mmi\":5.6399999999999997,\"alert\":null,\"status\":\"reviewed\",\"tsunami\":0,\"sig\":623,\"net\":\"nc\",\"code\":\"73286930\",\"ids\":\",nc73286930,us70005r3k,\",\"sources\":\",nc,us,\",\"types\":\",dyfi,focal-mechanism,geoserve,moment-tensor,nearby-cities,origin,phase-data,scitech-link,shakemap,\",\"nst\":233,\"dmin\":0.030450000000000001,\"rms\":0.089999999999999997,\"gap\":97,\"magType\":\"mw\",\"type\":\"earthquake\",\"title\":\"M 3.5 - 5km WSW of Colma, CA\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-122.5145,37.659833300000003,7.4500000000000002]},\"id\":\"nc73286930\"},\n" +
-                "{\"type\":\"Feature\",\"properties\":{\"mag\":5,\"place\":\"71km WNW of Talkeetna, Alaska\",\"time\":1570192083205,\"updated\":1570538292705,\"tz\":-540,\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/ak019cqb6c9o\",\"detail\":\"https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/ak019cqb6c9o.geojson\",\"felt\":589,\"cdi\":4.9000000000000004,\"mmi\":3.7400000000000002,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":1,\"sig\":673,\"net\":\"ak\",\"code\":\"019cqb6c9o\",\"ids\":\",at00pyunyq,ak019cqb6c9o,us70005qnc,\",\"sources\":\",at,ak,us,\",\"types\":\",dyfi,geoserve,ground-failure,impact-link,losspager,moment-tensor,oaf,origin,phase-data,shakemap,\",\"nst\":null,\"dmin\":null,\"rms\":0.58999999999999997,\"gap\":null,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 5.0 - 71km WNW of Talkeetna, Alaska\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-151.52000000000001,62.478400000000001,90.700000000000003]},\"id\":\"ak019cqb6c9o\"}],\"bbox\":[-151.52,37.6598333,7.45,-122.5145,62.4784,90.7]}";
+        JsonObject response = new JsonObject();
+
+        APIConnect apiConnect = new APIConnect();
+        try{
+             response = apiConnect.apiConnection();
+        }
+        catch (Exception e){
+            System.out.println("Problem with connection");
+        }
 
         Gson gson = new Gson();
 
